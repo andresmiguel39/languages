ebfb481058d7e70279418ffac6ce1d9f4fb5ccf6
andresmiguel39 <56973050+andresmiguel39@users.noreply.github.com>
2020-11-19 08:47:50 -0500

    Update script.sh

--- bash/script.sh
+++ bash/script.sh
@@ -9,3 +9,12 @@ echo $variable2
 echo $variable 
 
 echo "testing be `date` and $variable"
+
+#check if variable is null
+my_var="nixCraft"
+if [ -z "$my_var" ]
+then
+      echo "\$my_var is NULL"
+else
+      echo "\$my_var is NOT NULL"
+fi
