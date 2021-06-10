9473f45337e78399350b4761ba507e42b5c5387f
andresmiguel39 <56973050+andresmiguel39@users.noreply.github.com>
2021-06-10 16:09:22 -0500

    minimo
    minomo

--- python/syntax.py
+++ python/syntax.py
@@ -3,10 +3,27 @@ def make_negative( number ):
         number = "-"+ str(number)    
     return int(number)
     
-    make_negative( 5 )
+make_negative( 5 )
+
+
+make_negative( 0 )
+
+
+make_negative( -1 )
+
     
     
-    make_negative( 0 )
     
+def find_smallest_int(arr):
+    arr.sort()
+    return arr[0]
+
+find_smallest_int([78, 56, 232, 12, 11, 43])
+
+find_smallest_int([78, 56, -2, 12, 8, -33])
+
+find_smallest_int([0, 1-2**64, 2**64])
+
     
-    make_negative( -1 )
+def findSmallestInt(arr):
+    return min(arr)
