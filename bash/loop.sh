fe893b1753054d4e87baf07b31f584ffb0dde7fe
andresmiguel39 <56973050+andresmiguel39@users.noreply.github.com>
2022-05-05 11:39:47 -0500

    Update loop.sh

--- bash/loop.sh
+++ bash/loop.sh
@@ -13,3 +13,16 @@ while read line
 do 
     echo $line
 done < README.txt
+
+# infinite loop
+while 1 :
+do
+    echo $SECONDS
+done
+
+# timed loop
+# +1 will define how many time the loop will last
+end=$((SECONDS+1))
+while [ $SECONDS -lt $end ]; do
+    echo $SECONDS
+done
